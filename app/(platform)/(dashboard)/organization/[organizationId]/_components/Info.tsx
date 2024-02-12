51e900c165965f00bc3cc9af7b5e7aeb55d7c68f
hasnaat-iftikhar <hasnaatfreelancing@gmail.com>
2024-02-12 16:11:33 +0500

    :bug: Bug - Bug fixing in skeleton

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/Info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/Info.tsx
@@ -14,7 +14,7 @@ const Info = ({
 }) => {
     const { organization: activeOrganization, isLoaded: isActiveOrganizationLoaded } = useOrganization();
 
-    if(!isActiveOrganizationLoaded) <Info.Skeleton />
+    if(!isActiveOrganizationLoaded) return <Info.Skeleton className={className} />;
 
     return (
         <div className={twMerge(className, "flex items-center gap-x-3")}>
@@ -39,14 +39,16 @@ const Info = ({
     )
 }
 
-Info.Skeleton = function SkeletonInfo() {
+Info.Skeleton = function SkeletonInfo({
+    className=""
+}) {
     return (
-        <div className="flex items-center gap-x-4">
-            <div className="w-[60px] h-[60px] relative">
+        <div className={twMerge(className, "flex items-center gap-x-4")}>
+            <div className="w-[46px] h-[46px] relative">
                 <Skeleton className="w-full h-full absolute" />
             </div>
             <div className="space-y-2">
-                <Skeleton className="h-10 w-[200px]" />
+                <Skeleton className="h-[20px] w-[200px]" />
                 <div className="flex items-center">
                     <Skeleton className="h-4 w-4 mr-2" />
                     <Skeleton className="h-4 w-[100px]" />
